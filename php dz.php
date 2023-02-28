26123cdc84d69de43ca382bf2f534d77a854be24
alevur <alevure@yandex.ru>
2023-02-28 20:44:54 +0400

    git))

---
+++ php dz.php
@@ -0,0 +1,13 @@
+<?php
+$x=10;
+$firstArray=[];
+for ($i = 0; $i < 10; $i++) {
+    $firstArray[] = $x;
+}
+var_dump($firstArray);
+
+$array =[];
+for ($n = 0; $n < 10; $n++){
+    ;
+    $firstArray[] =rand(1,20);
+}  var_dump($array);
